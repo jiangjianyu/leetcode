f2c52dc4d4f5a295cf417a1965b8637166607345
Jiangjianyu <982503447@qq.com>
2019-10-29 22:27:01 +0800

    use python to realize leetcode018 add print

--- leetcode013.py
+++ leetcode013.py
@@ -2,6 +2,15 @@
 class Solution:
     def romanToInt(self, s):
         d = {'I':1, 'IV':3, 'V':5, 'IX':8, 'X':10, 'XL':30, 'L':50, 'XC':80, 'C':100, 'CD':300, 'D':500, 'CM':800, 'M':1000}
+        for i, n in enumerate(s):
+            print("i:",i)
+            print("n:",n)
+            print("s[max(i-1,0):i+1]:",s[max(i-1,0):i+1])
+            print("d[n]:",d[n])
+            print("d.get(s[max(i-1,0):i+1],d[n]):",d.get(s[max(i-1,0):i+1],d[n]))
+
+            print("------------------")
+
         return sum(d.get(s[max(i-1,0):i+1],d[n]) for i, n in enumerate(s))
 
 if __name__ == '__main__':
